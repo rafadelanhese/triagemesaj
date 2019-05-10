cdcf5399cfcad9e50902caa201fb7d16158df488
Rafael Delanhese Machado <hepta.rafael.d@AGU.GOV.BR>
2019-05-10 14:59:02 -0300

    Salvar em arquivos processos não triados

--- src/main/java/br/com/delanhese/triagemesaj/service/ArquivoService.java
+++ src/main/java/br/com/delanhese/triagemesaj/service/ArquivoService.java
@@ -12,18 +12,19 @@ import javax.swing.JOptionPane;
 
 public class ArquivoService {
     
-    private static final String ARQUIVO_TRIADO_PATH = "c:\\triagem\\arquivo-triado.csv";
+    private static final String ARQUIVO_TRIADO_PATH = "c:\\triagem\\processos-triado.csv";
+    private static final String ARQUIVO_NAO_TRIADO_PATH = "c:\\triagem\\processos-nao-triado.csv";
 
     public ArquivoService() {
     }
 
     public void executarTriagem(List<Processo> listaProcesso, List<PalavraChave> listaPalavraChave) {
         List<ProcessoClassificado> listaProcClassificado = new ArrayList<ProcessoClassificado>();
-        
+        List<Processo> listaProcNaoClassificado = new ArrayList<Processo>();
+        boolean classificado = false;
         for (Processo processo : listaProcesso) {
             for (PalavraChave palavra : listaPalavraChave) {
-                if (processo.getTeor().contains(palavra.getParametro())) {
-                    //FALTA VERIFICAR SE O PROCESSO FOI TRIADO MAIS DE UMA VEZ
+                if (processo.getTeor().contains(palavra.getParametro()) && !classificado) {                    
                     listaProcClassificado.add(new ProcessoClassificado(processo.getNumeroProcesso(),
                             processo.getForo(),
                             processo.getVara(),
@@ -39,29 +40,53 @@ public class ArquivoService {
                             palavra.getPrazo(),
                             palavra.getEtiqueta()));
 
+                            classificado = true;
+
                 }
             }
+
+            if(!classificado)
+                listaProcNaoClassificado.add(processo);
         }
-        gravaListaNoArquivoTriado(listaProcClassificado);
-        
-    }
-    private void gravaListaNoArquivoTriado(List<ProcessoClassificado> listaProcClassificado){
-        if (!listaProcClassificado.isEmpty()) {
-            try {
-                Writer writer = new FileWriter(ARQUIVO_TRIADO_PATH);
-                writer.write("Número do processo;Foro;Vara;Disponibilização;Prazo processual;Movimentação;Teor da intimação;Classe principal;Assunto principal;Situação;MODALIDADE;REPERCUSSÃO;PRAZO;ETIQUETA\n");
-                for (ProcessoClassificado pc : listaProcClassificado) {
-                    writer.write(formataStringDeGravacaoNoArquivoClassificado(pc));
-                }
-                writer.close();
-            } catch (IOException io) {
-                JOptionPane.showMessageDialog(null, "Erro ao gravar no arquivo de processos classificados");
-            } 
+        if (!listaProcClassificado.isEmpty()){
+            gravaListaNoArquivoTriado(listaProcClassificado);
         } else {
             JOptionPane.showMessageDialog(null, "Nenhum processo foi classificado");
         }
+
+        if (!listaProcNaoClassificado.isEmpty()) {
+            gravaListaNoArquivoNaoTriado(listaProcNaoClassificado)
+        } else {
+            JOptionPane.showMessageDialog(null, "Não há processo não classificados");
+        }        
     }
-    private String formataStringDeGravacaoNoArquivoClassificado(ProcessoClassificado pc){
+    private void gravaListaNoArquivoTriado(List<ProcessoClassificado> listaProcClassificado){        
+        try {
+            Writer writer = new FileWriter(ARQUIVO_TRIADO_PATH);
+            writer.write("Número do processo;Foro;Vara;Disponibilização;Prazo processual;Movimentação;Teor da intimação;Classe principal;Assunto principal;Situação;MODALIDADE;REPERCUSSÃO;PRAZO;ETIQUETA\n");
+            for (ProcessoClassificado pc : listaProcClassificado) {
+                writer.write(formataStringDeGravacao(pc));
+            }
+            writer.close();
+        } catch (IOException io) {
+            JOptionPane.showMessageDialog(null, "Erro ao gravar no arquivo de processos classificados");
+        } 
+    }
+
+    private void gravaListaNoArquivoNaoTriado(List<Processo> listaProcNaoClassificado){        
+        try {
+            Writer writer = new FileWriter(ARQUIVO_NAO_TRIADO_PATH);
+            writer.write("Número do processo;Foro;Vara;Disponibilização;Prazo processual;Movimentação;Teor da intimação;Classe principal;Assunto principal;Situação\n");
+            for (Processo processo : listaProcNaoClassificado) {
+                writer.write(processo.toString());
+            }
+            writer.close();
+        } catch (IOException io) {
+            JOptionPane.showMessageDialog(null, "Erro ao gravar no arquivo de processos não classificados");
+        }         
+    }
+
+    private String formataStringDeGravacao(ProcessoClassificado pc){
         return pc.getNumeroProcesso()+ ";"
                             +pc.getForo() + ";"
                             +pc.getVara()+ ";"
